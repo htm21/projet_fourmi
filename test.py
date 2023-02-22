582a0a9d19e7594fd0997756db264ab637c5dd84
Ahmad HATOUM <ahmad.hatoum@ens.uvsq.fr>
2023-02-22 11:48:33 +0100

    création de la fenêtre

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-svjkkfhsdhdhd
+import tkinter as tk
+
+
+racine = tk.Tk()
+racine.title("La Fourmi de Langton")
+
+Canvas1 = tk.Canvas(racine, height=700, width=700, bg="grey")
+Canvas1.pack(padx=15,pady=15)
