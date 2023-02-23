a769173029a55df19952a2fe4b91106b61cae48d
Ahmad HATOUM <ahmad.hatoum@ens.uvsq.fr>
2023-02-23 11:46:36 +0100

    Ajout de liste en 2D (grille initiale)

--- test.py
+++ test.py
@@ -4,6 +4,18 @@ import tkinter as tk
 HEIGHT = 700
 WIDTH = 700
 
+nombre_case = 50
+
+
+#Initiation liste 2D de la grille
+
+cases = []
+for ligne in range(nombre_case):
+    sous_liste = []
+    for colonne in range(nombre_case) :             #liste en 2D qui contient 50 lignes et 50 colonnes
+        sous_liste.append(0)
+    cases.append(sous_liste)
+
 
 
 
